2730eb0cfbb5ca3f6ced577e1be5200a6405b455
Yuka Solano <yukakyushima@gmail.com>
2023-02-04 20:12:02 -0300

    Create task list page

---
+++ lib/task_item.dart
@@ -0,0 +1,42 @@
+import 'package:coconut/task.dart';
+import 'package:flutter/material.dart';
+
+class TaskItem extends StatefulWidget {
+
+  Task task;
+
+  TaskItem({Key? key, required this.task})
+      : super(key: key);
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      key: Key('${widget.task.id}'),
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: colorScheme.primary.withOpacity(0.15),
+          ),
+          height: 80,
+          child: Row(
+            children: [
+              Checkbox(
+                  value: widget.task.checked,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      widget.task.checked = value!;
+                    });
+                  }),
+              Text(widget.task.name),
+            ],
+          )),
+    );
+  }
+}
---
+++ lib/tasks_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:coconut/tasks_list.dart';
+import 'package:flutter/material.dart';
+
+class TasksListPage extends StatelessWidget {
+  const TasksListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My tasks"),
+      ),
+      body: const TasksList(),
+    );
+  }
+}
