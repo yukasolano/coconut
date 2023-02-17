306f9f3ab48f4d9cfa0621f1fc6f93a1c512fa3e
Yuka Solano <yukakyushima@gmail.com>
2023-02-17 16:11:46 -0300

    Create backend service

---
+++ coconut-tasks/src/test/java/com/yukakyushima/coconuttasks/CoconutTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukakyushima.coconuttasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoconutTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
