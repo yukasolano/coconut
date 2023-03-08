2e87301bbc9f12f9a936364d2d5047420c5258c2
Yuka Solano <yukakyushima@gmail.com>
2023-03-08 11:48:04 -0300

    Create api tasks/sample

--- coconut-tasks/src/main/java/com/yukakyushima/coconuttasks/controller/TasksController.java
+++ coconut-tasks/src/main/java/com/yukakyushima/coconuttasks/controller/TasksController.java
@@ -1,13 +1,16 @@
 package com.yukakyushima.coconuttasks.controller;
 
 import com.yukakyushima.coconuttasks.controller.dto.TaskDto;
+import com.yukakyushima.coconuttasks.model.Task;
 import com.yukakyushima.coconuttasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -17,7 +20,20 @@ public class TasksController {
     private TaskRepository taskRepository;
 
     @GetMapping
-    public List<TaskDto> getAll() {
+    public List<TaskDto> findAll() {
+        return taskRepository.findAll().stream().map(TaskDto::toDto).toList();
+    }
+
+    @GetMapping("/sample")
+    public List<TaskDto> sample() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(UUID.randomUUID(), "Item 1", false));
+        tasks.add(new Task(UUID.randomUUID(), "Item 2", false));
+        tasks.add(new Task(UUID.randomUUID(), "Item 3", false));
+        tasks.add(new Task(UUID.randomUUID(), "Item 4", false));
+        tasks.add(new Task(UUID.randomUUID(), "Item 5", false));
+        tasks.add(new Task(UUID.randomUUID(), "Item 6", false));
+        taskRepository.saveAll(tasks);
         return taskRepository.findAll().stream().map(TaskDto::toDto).toList();
     }
 }
--- coconut-tasks/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO TASK(id, name, checked)VALUES(UNHEX(REPLACE(UUID(),"-","")), 'Tarefa 1', 0);
