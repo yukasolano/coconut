839a907cfee39666b6a948511f62ec75b39b81c3
Yuka Solano <yukakyushima@gmail.com>
2023-03-08 15:13:47 -0300

    Connect with database on docker

--- mysql8/database.sql
+++ mysql8/database.sql
@@ -1,2 +1,6 @@
+CREATE TABLE `coconut`.`task`(`id` binary(16) NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+	`checked` bit(1) NOT NULL,
+	PRIMARY KEY (`id`));
 GRANT ALL PRIVILEGES ON *.* TO 'coconut'@'%';
 FLUSH PRIVILEGES;
